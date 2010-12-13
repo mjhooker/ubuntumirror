6a49d9695016690b01dea477690dafe471b5aa19
mjhooker <mjhooker@dell-server.(none)>
2010-12-13 04:17:04 +0000

    changes to handle proxy

--- debmirror.bash
+++ debmirror.bash
@@ -4,4 +4,4 @@ touch ~/.gnupg/trustedkeys.gpg
 chmod 600 ~/.gnupg/trustedkeys.gpg
 gpg --keyserver pgp.mit.edu --primary-keyring ~/.gnupg/trustedkeys.gpg --recv-keys 437D05B5
 
-debmirror --nosource --diff=none --progress --verbose --method=http --host gb.archive.ubuntu.com --root=ubuntu --dist=lucid,lucid-updates,lucid-security --section=main,restricted,multiverse,universe ubuntu
+debmirror --proxy=http://192.168.0.17:3128 --nosource --diff=none --progress --verbose --method=http --host gb.archive.ubuntu.com --root=ubuntu --dist=lucid,lucid-updates,lucid-security --section=main,restricted,multiverse,universe ubuntu
