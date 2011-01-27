fd68f9f072338531fd827d3a15b5c2088ad41818
mjhooker <mjhooker@dell-server.(none)>
2011-01-27 09:39:45 +0000

    use proxy and repeat downloads

--- debmirror.bash
+++ debmirror.bash
@@ -4,4 +4,10 @@ touch ~/.gnupg/trustedkeys.gpg
 chmod 600 ~/.gnupg/trustedkeys.gpg
 gpg --keyserver pgp.mit.edu --primary-keyring ~/.gnupg/trustedkeys.gpg --recv-keys 437D05B5
 
-debmirror --proxy=http://192.168.0.17:3128 --nosource --diff=none --progress --verbose --method=http --host gb.archive.ubuntu.com --root=ubuntu --dist=lucid,lucid-updates,lucid-security --section=main,restricted,multiverse,universe ubuntu
+while true
+do
+
+debmirror --proxy=http://192.168.0.15:3128 --nosource --diff=none --progress --verbose --method=http --host gb.archive.ubuntu.com --root=ubuntu --dist=lucid,lucid-updates,lucid-security --section=main,restricted,multiverse,universe ubuntu
+#debmirror --nosource --diff=none --progress --verbose --method=http --host gb.archive.ubuntu.com --root=ubuntu --dist=lucid,lucid-updates,lucid-security --section=main,restricted,multiverse,universe ubuntu
+sleep 600
+done
