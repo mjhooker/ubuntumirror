c11b4c7567456cc784b87bcac116be48fcf0f1b7
mjhooker <mjhooker@dell-server.(none)>
2011-02-12 01:25:51 +0000

    squid adjustments

--- debmirror.bash
+++ debmirror.bash
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-touch ~/.gnupg/trustedkeys.gpg
-chmod 600 ~/.gnupg/trustedkeys.gpg
-gpg --keyserver pgp.mit.edu --primary-keyring ~/.gnupg/trustedkeys.gpg --recv-keys 437D05B5
+#touch ~/.gnupg/trustedkeys.gpg
+#chmod 600 ~/.gnupg/trustedkeys.gpg
+#gpg --keyserver pgp.mit.edu --primary-keyring ~/.gnupg/trustedkeys.gpg --recv-keys 437D05B5
 
 while true
 do
 
-debmirror --proxy=http://192.168.0.15:3128 --nosource --diff=none --progress --verbose --method=http --host gb.archive.ubuntu.com --root=ubuntu --dist=lucid,lucid-updates,lucid-security --section=main,restricted,multiverse,universe ubuntu
-#debmirror --nosource --diff=none --progress --verbose --method=http --host gb.archive.ubuntu.com --root=ubuntu --dist=lucid,lucid-updates,lucid-security --section=main,restricted,multiverse,universe ubuntu
+#debmirror --proxy=http://192.168.0.3:3128 --nosource --diff=none --progress --verbose --method=http --host gb.archive.ubuntu.com --root=ubuntu --dist=lucid,lucid-updates,lucid-security --section=main,restricted,multiverse,universe ubuntu
+debmirror --nosource --diff=none --progress --verbose --method=http --host gb.archive.ubuntu.com --root=ubuntu --dist=lucid,lucid-updates,lucid-security --section=main,restricted,multiverse,universe ubuntu
 sleep 600
 done
